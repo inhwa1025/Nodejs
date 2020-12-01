b2815333e3907f393abffb90f492c943d9d3f5c1
inhwa1025 <inhwa9901@gmail.com>
2020-12-01 23:49:59 +0900

    refactoring

--- main.js
+++ main.js
@@ -3,35 +3,41 @@ var fs = require('fs');
 var url = require('url');
 var qs = require("querystring");
 
-function templateHTML(title, list, body, control){
-  return `
-  <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    ${list}
-    ${control}
-    ${body}
-  </body>
-  </html>
-  `;
+//refactoring
+var template = {
+  HTML:function(title, list, body, control){
+    return `
+    <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      ${list}
+      ${control}
+      ${body}
+    </body>
+    </html>
+    `;
+  }, 
+  list:function(filelist){
+    var list = '<ul>';
+    var i = 0;
+    while(i < filelist.length) {
+     list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+      i++;
+    }
+    list += '</ul>';
+    return list;
+  }
+
 }
 
-function templateList(filelist){
-  var list = '<ul>';
-  var i = 0;
-  while(i < filelist.length) {
-   list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-    i++;
-  }
-  list += '</ul>';
-  return list;
-}
+
  
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -43,21 +49,21 @@ var app = http.createServer(function(request,response){
         fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var list = templateList(filelist);
-          var template = templateHTML(title, list, 
+          var list = template.list(filelist);
+          var html = template.HTML(title, list, 
             `<h2>${title}</h2>${description}`, //body
             `<a href="/create">create</a>` //control
             );
           response.writeHead(200); //200 : 성공적으로 파일 전송
-          response.end(template);
+          response.end(html);
         })
           
       } else {
         fs.readdir('./data', function(error, filelist){
-          var list = templateList(filelist);
+          var list = template.list(filelist);
           fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
             var title = queryData.id;
-            var template = templateHTML(title, list, 
+            var html = template.HTML(title, list, 
               `<h2>${title}</h2>${description}`, //body
               ` <a href="/create">create</a>
                 <a href="/update?id=${title}">update</a>
@@ -67,15 +73,15 @@ var app = http.createServer(function(request,response){
                 </form>` //control
               );
             response.writeHead(200); //200 : 성공적으로 파일 전송
-            response.end(template);
+            response.end(html);
           });
         });
       }
     } else if(pathname === '/create'){
       fs.readdir('./data', function(error, filelist){
         var title = 'WEB - create';
-        var list = templateList(filelist);
-        var template = templateHTML(title, list, `
+        var list = template.list(filelist);
+        var html = template.HTML(title, list, `
         <form action="/create_process" method="post"> 
           <p><input type="text" name="title" placeholder="title"></p>
           <p>
@@ -89,7 +95,7 @@ var app = http.createServer(function(request,response){
         `` //control
         );
         response.writeHead(200); //200 : 성공적으로 파일 전송
-        response.end(template);
+        response.end(html);
       })
     } else if(pathname === "/create_process") {
       var body = "";
@@ -105,7 +111,7 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
         //더 이상 들어올 데이터가 없을 때 콜백함수 호출
-        var post = qs.parse(body); //지금껏 저장한 body를 입력값으로. -> dic형태로 됨.
+        var post = qs.parse(body); //지금껏 저장한 body를 입력값으로. -> 객체로 저장됨
         var title = post.title;
         var description = post.description;
         fs.writeFile(`data/${title}`, description, 'utf8', function(err){
@@ -116,10 +122,10 @@ var app = http.createServer(function(request,response){
       });
     } else if(pathname === '/update') {
       fs.readdir('./data', function(error, filelist){
-        var list = templateList(filelist);
+        var list = template.list(filelist);
         fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
           var title = queryData.id;
-          var template = templateHTML(title, list, 
+          var html = template.HTML(title, list, 
             `
             <form action="/update_process" method="post"> 
               <input type="hidden" name="id" value="${title}"> 
@@ -138,7 +144,7 @@ var app = http.createServer(function(request,response){
             <a href="/update?id=${title}">update</a>` //control
             );
           response.writeHead(200); //200 : 성공적으로 파일 전송
-          response.end(template);
+          response.end(html);
         });
       });
     } else if(pathname === '/update_process'){
---
+++ syntax/object.js
@@ -0,0 +1,21 @@
+//array vs. object
+
+var members = ['egoing', 'k8805', 'hoya'];
+console.log(members[1]);
+var i = 0;
+while(i < members.length){
+    console.log(members[i]);
+    i += 1;
+}
+
+
+var roles = {
+    'programmer':'egoing',
+    'designer':'k8805',
+    'manager':'hoya'
+}
+console.log(roles.designer);
+console.log(roles['designer']);
+for(var name in roles){
+    console.log(roles[name]);
+}
